95d3d844f70f40c0d34e69d7b0450c8cc866bcd2
ghosttttttt <carl100897@gmail.com>
2019-06-07 17:42:55 +0200

    creation des entites

--- my-project/src/Controller/BlogController.php
+++ my-project/src/Controller/BlogController.php
@@ -8,7 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class BlogController extends AbstractController
+class
+BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog_index")
